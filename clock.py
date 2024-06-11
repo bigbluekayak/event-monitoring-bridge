11c6ff4d06220ceb0997aaf48196e11c2af5ee76
Martin Eley <meley@salesforce.com>
2024-06-11 15:37:07 +0100

    Changed clock timer to 60 min

--- clock.py
+++ clock.py
@@ -3,7 +3,7 @@ from apscheduler.schedulers.blocking import BlockingScheduler
 
 sched = BlockingScheduler()
 
-@sched.scheduled_job('interval', minutes=1)
+@sched.scheduled_job('interval', minutes=60)
 def timed_job():
     event_monitoring_logs.process.delay()
 
